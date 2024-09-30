cb13ad71a90c48881a7d8bdc3fb0a4abc7d57b22
M.Rafli Albukhori <rr5428820@gmail.com>
2024-09-30 13:16:21 +0700

    Test

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>
+        Hallo
+        Rafli
         Open up App.js to start working on your app! tes tes 123 aaaa{" "}
       </Text>
       <StatusBar style="auto" />
